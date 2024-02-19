64a4e59aaf16b185291d182e9e7b09ca75dc04bd
quarshie133 <wrightsprince@gmail.com>
2024-02-19 11:21:35 +0000

    fluttera_app setup

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App Title'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Center(
+          child: Center(
+            child: const Text('Body'),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(
+                Icons.home,
+              ),
+            ),
+            BottomNavigationBarItem(
+                label: 'Settings', icon: Icon(Icons.settings))
+          ],
+        ),
+      ),
+    );
+  }
+}
